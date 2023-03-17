d521790dc636b37feebabaad8b191f254d7308a3
Giovana <maiiaraoliveiras1@gmail.com>
2023-03-16 23:33:02 -0300

    Parte 3 - Relacionamento entre duas classes

--- src/main/java/com/generation/blogpessoal/controller/PostagemController.java
+++ src/main/java/com/generation/blogpessoal/controller/PostagemController.java
@@ -22,6 +22,7 @@ import org.springframework.web.server.ResponseStatusException;
 
 import com.generation.blogpessoal.model.Postagem;
 import com.generation.blogpessoal.repository.PostagemRepository;
+import com.generation.blogpessoal.repository.TemaRepository;
 
 @RestController
 @RequestMapping("/postagens")
@@ -30,6 +31,10 @@ public class PostagemController {
 
 	@Autowired
 	private PostagemRepository postagemRepository;
+
+	@Autowired
+	private TemaRepository temaRepository;
+
 
 	@GetMapping
 	public ResponseEntity<List<Postagem>> getAll() {
@@ -49,18 +54,24 @@ public class PostagemController {
 	}
 	
 	@PostMapping
-	public ResponseEntity<Postagem> post(@Valid @RequestBody Postagem postagem){
-		return ResponseEntity.status(HttpStatus.CREATED)
-				.body(postagemRepository.save(postagem));
-	}
+    public ResponseEntity<Postagem> post(@Valid @RequestBody Postagem postagem) {
+        if (temaRepository.existsById(postagem.getTema().getId()))
+            return ResponseEntity.status(HttpStatus.CREATED)
+                .body(postagemRepository.save(postagem));
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+    }
 	
-	@PutMapping
-	public ResponseEntity<Postagem> put(@Valid @RequestBody Postagem postagem){
-		return postagemRepository.findById(postagem.getId())
-				.map(resposta -> ResponseEntity.status(HttpStatus.OK)
-						.body(postagemRepository.save(postagem)))
-					.orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());		
-	}
+	 @PutMapping
+	    public ResponseEntity<Postagem> put(@Valid @RequestBody Postagem postagem) {
+	        if (postagemRepository.existsById(postagem.getId())) {
+	            if (temaRepository.existsById(postagem.getTema().getId()))
+	                return ResponseEntity.status(HttpStatus.OK)
+	                        .body(postagemRepository.save(postagem));
+	            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+	        }
+	        return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+	    }
 	
 	@ResponseStatus(HttpStatus.NO_CONTENT)
 	@DeleteMapping("/{id}")
